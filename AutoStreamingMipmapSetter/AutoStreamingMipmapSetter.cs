2f9e1ad9ca4cab8a5b7c200a94a678a3c875bc5a
thakyuu <thakyuu@users.noreply.github.com>
2020-05-13 17:13:57 +0900

    Initial Commit

---
+++ AutoStreamingMipmapSetter/AutoStreamingMipmapSetter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AutoStreamingMipmapSetter : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        TextureImporter Ti = assetImporter as TextureImporter;
+
+        if (!Ti.mipmapEnabled || Ti.streamingMipmaps)
+        {
+            return;
+        }
+
+        Ti.streamingMipmaps = true;
+    }
+}
